0d223ef45f5f9dad2e3a7e2b1c63a8bb1b3a4be0
Futa Watanabe <codingtrain9@gmail.com>
2025-06-07 20:43:53 +0900

    update experienceData

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -10,21 +10,11 @@ export interface Experience {
 
 export const experienceData: Experience[] = [
   {
-    date: "Summer 2023",
-    title: "Research Intern",
-    company: "DeepMind",
+    date: "Summer 2024",
+    title: "Summer Internship",
+    company: "Jij",
     description:
-      "Developed novel algorithms for causal structure learning in reinforcement learning settings",
-    advisor: "Peter Wang",
-    companyUrl: "https://deepmind.com",
-  },
-  {
-    date: "Summer 2022",
-    title: "Research Intern",
-    company: "Google Research",
-    description:
-      "Worked on improving robustness of large language models to distribution shifts",
-    manager: "Elise Brown",
-    companyUrl: "https://google.com",
+      "Implemented a quantum optimization algorithm using Jij's SDK Qamomile.",
+    companyUrl: "https://www.j-ij.com/ja",
   },
 ];
