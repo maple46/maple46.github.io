2586c49ded76209d75d8664d8b11f4bdd430d443
Futa Watanabe <codingtrain9@gmail.com>
2025-06-07 20:37:05 +0900

    update portfolio

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -9,6 +9,7 @@ export interface Portfolio {
 
 export const portfolioData: Portfolio[] = [
   // Example entry
+  /*
   {
     title: "Causal Discovery Framework",
     description:
@@ -19,4 +20,5 @@ export const portfolioData: Portfolio[] = [
       "https://images.unsplash.com/photo-1561622539-dffbfc2008fd?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     codeUrl: "https://github.com/username/project",
   },
+  */
 ];
