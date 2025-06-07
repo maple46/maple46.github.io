7dce13d67b58df0e4d01078850c441f5b093593d
Futa Watanabe <codingtrain9@gmail.com>
2025-06-07 21:23:37 +0900

    update aboutme

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -18,22 +18,22 @@ export interface AboutMe {
 }
 
 export const aboutMe: AboutMe = {
-  name: "Jane R. Smith",
-  title: "Ph.D. Candidate",
-  institution: "Stanford University",
+  name: "Futa Watanabe",
+  title: "Undergraduate student",
+  institution: "Institute of Science Tokyo",
   // Note that links work in the description
   description:
-    "I'm a final-year <a href='https://www.stanford.edu'>PhD candidate</a> working at the intersection of causal inference and machine learning. My research focuses on developing robust, interpretable systems that can reason about cause and effect in complex environments.",
-  email: "______@stanford.edu",
+    "I'm a forth-year <a href='https://www.isct.ac.jp/en'>undergraduate student</a> working at Narural Language Processing.",
+  email: "",
   imageUrl:
-    "https://images.unsplash.com/photo-1581481615985-ba4775734a9b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  googleScholarUrl: "https://scholar.google.com/citations?user=bWtMl_MAAAAJ",
-  githubUsername: "janesmith",
-  linkedinUsername: "janesmith",
-  twitterUsername: "janesmith",
-  blogUrl: "https://",
-  cvUrl: "https://",
-  institutionUrl: "https://www.stanford.edu",
+    "",
+  googleScholarUrl: "",
+  githubUsername: "maple46",
+  linkedinUsername: "",
+  twitterUsername: "maple_46_",
+  blogUrl: "",
+  cvUrl: "",
+  institutionUrl: "https://www.isct.ac.jp/en",
   // altName: "",
   // secretDescription: "I like dogs.",
 };
